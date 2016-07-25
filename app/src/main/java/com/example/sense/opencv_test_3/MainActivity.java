d243b959d68972a7b6662b83730470dd4f0af0ee
Sebastian Ciscar <sensenom69@gmail.com>
2016-07-25 11:24:52 +0200

    Bruixola fixa, pareix que no senvaja massa segons els compas del movil

--- app/src/main/java/com/example/sense/opencv_test_3/MainActivity.java
+++ app/src/main/java/com/example/sense/opencv_test_3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sense.opencv_test_3;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
@@ -67,6 +68,9 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     //La brujula
     private SensorManager sensorManager;
     private MyBrujula brujula;
+    //LA nova bruixola
+    private OrientationSensor novaBrujula;
+
     // Views donde se cargaran los elementos del XML
     private TextView txtAngle;
     private ImageView imgCompass;
@@ -139,6 +143,8 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         sensorManager = brujula.getmSensorManager();
         //////
 
+
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.show_camera);
@@ -177,9 +183,9 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 longitud = mlocListener.longitud;
                 angle = brujula.getAngle();
                 //Laboratori
-                latitut = 39.070622;
-                longitud = -0.269588;
-                angle = 177.5;//moduver desde laboratori
+                //latitut = 39.070622;
+                //longitud = -0.269588;
+                //angle = 177.5;//moduver desde laboratori
                 //Casa
                 //latitut = 39.068727;
                 //longitud = -0.291360;
@@ -213,6 +219,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         super.onResume();
 
         // Se registra un listener para los sensores del accelerometer y el             magnetometer
+        sensorManager.registerListener(brujula,brujula.rotationmeter,SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(brujula, brujula.accelerometer, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(brujula, brujula.magnetometer, SensorManager.SENSOR_DELAY_UI);
 
@@ -415,6 +422,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         // Los dos sensores que son necesarios porque TYPE_ORINETATION esta deprecated
         private Sensor accelerometer;
         private Sensor magnetometer;
+        private Sensor rotationmeter;
 
         // Los angulos del movimiento de la flecha que señala al norte
         float degree;
@@ -425,16 +433,21 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         // Guarda los valores que cambián con las variaciones del sensor TYPE_MAGNETIC_FIELD
         float[] mGeomagnetic;
         //proves
-        float[] matriu = new float[9];
+        float[] rotationVector;
+        float pich;
+        float avgRead[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
 
         public void llansa(){
             // Se inicializa los sensores del dispositivo android
             mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            rotationmeter = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
             accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
             mGravity = null;
             mGeomagnetic = null;
+            rotationVector = null;
         }
 
         public SensorManager getmSensorManager(){
@@ -449,24 +462,52 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                     mGravity = lowPass( event.values.clone(), mGravity );
             if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
                     mGeomagnetic = lowPass( event.values.clone(), mGeomagnetic );
+            if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+                rotationVector = event.values.clone();
 
 
 
             if ((mGravity != null) && (mGeomagnetic != null)) {
-                float RotationMatrix[] = new float[16];
+                float rotationV[] = new float[16];
                 float RI[] = new float[9];
                 float I[] = new float[9];
-                boolean success = SensorManager.getRotationMatrix(RI, null, mGravity, mGeomagnetic);
+                boolean success = SensorManager.getRotationMatrix(RI, I, mGravity, mGeomagnetic);
 
                 if (success) {
                     float orientation[] = new float[3];
-                    SensorManager.getOrientation(RI, orientation);
-                    //azimut = orientation[0] * (180 / (float) Math.PI);
-                    azimut = (float) Math.toDegrees(orientation[0]);
+                    //SensorManager.getOrientation(RI, orientation);
+                    float[] outR = new float[9];
+                    SensorManager.remapCoordinateSystem(RI, SensorManager.AXIS_X,SensorManager.AXIS_Z, outR);
+                    SensorManager.getOrientation(outR, orientation);
+
+                    azimut = orientation[0] * (180 / (float) Math.PI);
+                    pich = orientation[1] * 180/ (float) Math.PI;
+                    //azimut = (float) Math.toDegrees(orientation[0]);
+
+                    avgRead[0] = avgRead[1];
+                    avgRead[1] = avgRead[2];
+                    avgRead[2] = avgRead[3];
+                    avgRead[3] = avgRead[4];
+                    avgRead[4] = avgRead[5];
+                    avgRead[5] = avgRead[6];
+                    avgRead[6] = avgRead[7];
+                    avgRead[7] = avgRead[8];
+                    avgRead[8] = orientation[0]; // orientation contains: azimuth, pitch and roll
+                    azimut = (avgRead[0] + avgRead[1] + avgRead[2] + avgRead[3]
+                            + avgRead[4] + avgRead[5] + avgRead[6] + avgRead[7] + avgRead[8]) / 9;
+
+
+                    azimut = azimut * 360 / (2 * 3.14159f);
+                    if (azimut < 0 && azimut > -180)
+                        azimut += 360;
+
                 }
+                txtAngle.setText("Angle: " + azimut);
+
             }
+
             degree = azimut;
-            txtAngle.setText("Angle: " + Math.round((degree)) + " degrees");
+
             // se crea la animacion de la rottacion (se revierte el giro en grados, negativo)
 
             RotateAnimation ra = new RotateAnimation(
@@ -647,6 +688,150 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 llistaPunts.add(new Punt(latitud, longitud, altura));
             }
             return null;
+        }
+    }
+
+    //PRova de bruixla nova
+    public static class OrientationSensor implements  SensorEventListener {
+
+        public final static int SENSOR_UNAVAILABLE = -1;
+
+        // references to other objects
+        SensorManager m_sm;
+        SensorEventListener m_parent;   // non-null if this class should call its parent after onSensorChanged(...) and onAccuracyChanged(...) notifications
+        Activity m_activity;            // current activity for call to getWindowManager().getDefaultDisplay().getRotation()
+
+        // raw inputs from Android sensors
+        float m_Norm_Gravity;           // length of raw gravity vector received in onSensorChanged(...).  NB: should be about 10
+        float[] m_NormGravityVector;    // Normalised gravity vector, (i.e. length of this vector is 1), which points straight up into space
+        float m_Norm_MagField;          // length of raw magnetic field vector received in onSensorChanged(...).
+        float[] m_NormMagFieldValues;   // Normalised magnetic field vector, (i.e. length of this vector is 1)
+
+        // accuracy specifications. SENSOR_UNAVAILABLE if unknown, otherwise SensorManager.SENSOR_STATUS_UNRELIABLE, SENSOR_STATUS_ACCURACY_LOW, SENSOR_STATUS_ACCURACY_MEDIUM or SENSOR_STATUS_ACCURACY_HIGH
+        int m_GravityAccuracy;          // accuracy of gravity sensor
+        int m_MagneticFieldAccuracy;    // accuracy of magnetic field sensor
+
+        // values calculated once gravity and magnetic field vectors are available
+        float[] m_NormEastVector;       // normalised cross product of raw gravity vector with magnetic field values, points east
+        float[] m_NormNorthVector;      // Normalised vector pointing to magnetic north
+        boolean m_OrientationOK;        // set true if m_azimuth_radians and m_pitch_radians have successfully been calculated following a call to onSensorChanged(...)
+        float m_azimuth_radians;        // angle of the device from magnetic north
+        float m_pitch_radians;          // tilt angle of the device from the horizontal.  m_pitch_radians = 0 if the device if flat, m_pitch_radians = Math.PI/2 means the device is upright.
+        float m_pitch_axis_radians;     // angle which defines the axis for the rotation m_pitch_radians
+
+        public OrientationSensor(SensorManager sm, SensorEventListener parent) {
+            m_sm = sm;
+            m_parent = parent;
+            m_activity = null;
+            m_NormGravityVector = m_NormMagFieldValues = null;
+            m_NormEastVector = new float[3];
+            m_NormNorthVector = new float[3];
+            m_OrientationOK = false;
+        }
+
+        public int Register(Activity activity, int sensorSpeed) {
+            m_activity = activity;  // current activity required for call to getWindowManager().getDefaultDisplay().getRotation()
+            m_NormGravityVector = new float[3];
+            m_NormMagFieldValues = new float[3];
+            m_OrientationOK = false;
+            int count = 0;
+            Sensor SensorGravity = m_sm.getDefaultSensor(Sensor.TYPE_GRAVITY);
+            if (SensorGravity != null) {
+                m_sm.registerListener(this, SensorGravity, sensorSpeed);
+                m_GravityAccuracy = SensorManager.SENSOR_STATUS_ACCURACY_HIGH;
+                count++;
+            } else {
+                m_GravityAccuracy = SENSOR_UNAVAILABLE;
+            }
+            Sensor SensorMagField = m_sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            if (SensorMagField != null) {
+                m_sm.registerListener(this, SensorMagField, sensorSpeed);
+                m_MagneticFieldAccuracy = SensorManager.SENSOR_STATUS_ACCURACY_HIGH;
+                count++;
+            } else {
+                m_MagneticFieldAccuracy = SENSOR_UNAVAILABLE;
+            }
+            return count;
+        }
+
+        public void Unregister() {
+            m_activity = null;
+            m_NormGravityVector = m_NormMagFieldValues = null;
+            m_OrientationOK = false;
+            m_sm.unregisterListener(this);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent evnt) {
+            int SensorType = evnt.sensor.getType();
+            switch(SensorType) {
+                case Sensor.TYPE_GRAVITY:
+                    if (m_NormGravityVector == null) m_NormGravityVector = new float[3];
+                    System.arraycopy(evnt.values, 0, m_NormGravityVector, 0, m_NormGravityVector.length);
+                    m_Norm_Gravity = (float)Math.sqrt(m_NormGravityVector[0]*m_NormGravityVector[0] + m_NormGravityVector[1]*m_NormGravityVector[1] + m_NormGravityVector[2]*m_NormGravityVector[2]);
+                    for(int i=0; i < m_NormGravityVector.length; i++) m_NormGravityVector[i] /= m_Norm_Gravity;
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    if (m_NormMagFieldValues == null) m_NormMagFieldValues = new float[3];
+                    System.arraycopy(evnt.values, 0, m_NormMagFieldValues, 0, m_NormMagFieldValues.length);
+                    m_Norm_MagField = (float)Math.sqrt(m_NormMagFieldValues[0]*m_NormMagFieldValues[0] + m_NormMagFieldValues[1]*m_NormMagFieldValues[1] + m_NormMagFieldValues[2]*m_NormMagFieldValues[2]);
+                    for(int i=0; i < m_NormMagFieldValues.length; i++) m_NormMagFieldValues[i] /= m_Norm_MagField;
+                    break;
+            }
+            if (m_NormGravityVector != null && m_NormMagFieldValues != null) {
+                // first calculate the horizontal vector that points due east
+                float East_x = m_NormMagFieldValues[1]*m_NormGravityVector[2] - m_NormMagFieldValues[2]*m_NormGravityVector[1];
+                float East_y = m_NormMagFieldValues[2]*m_NormGravityVector[0] - m_NormMagFieldValues[0]*m_NormGravityVector[2];
+                float East_z = m_NormMagFieldValues[0]*m_NormGravityVector[1] - m_NormMagFieldValues[1]*m_NormGravityVector[0];
+                float norm_East = (float)Math.sqrt(East_x * East_x + East_y * East_y + East_z * East_z);
+                if (m_Norm_Gravity * m_Norm_MagField * norm_East < 0.1f) {  // Typical values are  > 100.
+                    m_OrientationOK = false; // device is close to free fall (or in space?), or close to magnetic north pole.
+                } else {
+                    m_NormEastVector[0] = East_x / norm_East; m_NormEastVector[1] = East_y / norm_East; m_NormEastVector[2] = East_z / norm_East;
+
+                    // next calculate the horizontal vector that points due north
+                    float M_dot_G = (m_NormGravityVector[0] *m_NormMagFieldValues[0] + m_NormGravityVector[1]*m_NormMagFieldValues[1] + m_NormGravityVector[2]*m_NormMagFieldValues[2]);
+                    float North_x = m_NormMagFieldValues[0] - m_NormGravityVector[0] * M_dot_G;
+                    float North_y = m_NormMagFieldValues[1] - m_NormGravityVector[1] * M_dot_G;
+                    float North_z = m_NormMagFieldValues[2] - m_NormGravityVector[2] * M_dot_G;
+                    float norm_North = (float)Math.sqrt(North_x * North_x + North_y * North_y + North_z * North_z);
+                    m_NormNorthVector[0] = North_x / norm_North; m_NormNorthVector[1] = North_y / norm_North; m_NormNorthVector[2] = North_z / norm_North;
+
+                    // take account of screen rotation away from its natural rotation
+                    int rotation = m_activity.getWindowManager().getDefaultDisplay().getRotation();
+                    float screen_adjustment = 0;
+                    switch(rotation) {
+                        case Surface.ROTATION_0:   screen_adjustment =          0;         break;
+                        case Surface.ROTATION_90:  screen_adjustment =   (float)Math.PI/2; break;
+                        case Surface.ROTATION_180: screen_adjustment =   (float)Math.PI;   break;
+                        case Surface.ROTATION_270: screen_adjustment = 3*(float)Math.PI/2; break;
+                    }
+                    // NB: the rotation matrix has now effectively been calculated. It consists of the three vectors m_NormEastVector[], m_NormNorthVector[] and m_NormGravityVector[]
+
+                    // calculate all the required angles from the rotation matrix
+                    // NB: see http://math.stackexchange.com/questions/381649/whats-the-best-3d-angular-co-ordinate-system-for-working-with-smartfone-apps
+                    float sin = m_NormEastVector[1] -  m_NormNorthVector[0], cos = m_NormEastVector[0] +  m_NormNorthVector[1];
+                    m_azimuth_radians = (float) (sin != 0 && cos != 0 ? Math.atan2(sin, cos) : 0);
+                    m_pitch_radians = (float) Math.acos(m_NormGravityVector[2]);
+                    sin = -m_NormEastVector[1] -  m_NormNorthVector[0]; cos = m_NormEastVector[0] -  m_NormNorthVector[1];
+                    float aximuth_plus_two_pitch_axis_radians = (float)(sin != 0 && cos != 0 ? Math.atan2(sin, cos) : 0);
+                    m_pitch_axis_radians = (float)(aximuth_plus_two_pitch_axis_radians - m_azimuth_radians) / 2;
+                    m_azimuth_radians += screen_adjustment;
+                    m_pitch_axis_radians += screen_adjustment;
+                    m_OrientationOK = true;
+                }
+            }
+            if (m_parent != null) m_parent.onSensorChanged(evnt);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            int SensorType = sensor.getType();
+            switch(SensorType) {
+                case Sensor.TYPE_GRAVITY: m_GravityAccuracy = accuracy; break;
+                case Sensor.TYPE_MAGNETIC_FIELD: m_MagneticFieldAccuracy = accuracy; break;
+            }
+            if (m_parent != null) m_parent.onAccuracyChanged(sensor, accuracy);
         }
     }
 
