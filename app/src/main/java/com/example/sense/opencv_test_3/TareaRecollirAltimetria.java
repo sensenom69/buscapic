99aa1038bbeac4e4d827c9609f682b5f44fe1dd3
Sebastian Ciscar <sensenom69@gmail.com>
2016-08-18 12:38:10 +0200

    Afegit el grafic de altimetria

--- app/src/main/java/com/example/sense/opencv_test_3/TareaRecollirAltimetria.java
+++ app/src/main/java/com/example/sense/opencv_test_3/TareaRecollirAltimetria.java
@@ -1,10 +1,17 @@
 package com.example.sense.opencv_test_3;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,6 +24,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by sense on 09/08/2016.
@@ -147,11 +155,49 @@ public class TareaRecollirAltimetria extends AsyncTask<Double,Integer,ArrayList<
             TextView textDistanciaPeu = (TextView) activity.findViewById(R.id.textDistanciaPeu);
             textDistanciaPeu.setText(Math.round(distanciaPeu*10)/10 + "m");
             TextView textDistancia = (TextView) activity.findViewById(R.id.textDistancia);
-            textDistancia.setText((30000/NUM_MIDES)*posPuntMesAlt + "mo");
+            textDistancia.setText((30000/NUM_MIDES)*posPuntMesAlt + "m");
+            //El grafic
+            //ArrayList<Entry> yVals = carregaDadesGrafic(llistaPunts);
+            ArrayList<String> xVals = new ArrayList<String>();
+            for(int i=0; i< llistaPunts.size() && i<posPuntMesAlt;i++)
+                xVals.add((30000/NUM_MIDES)*i+"");
+            LineChart chart = (LineChart) activity.findViewById(R.id.chart);
+            chart.setVisibility(View.VISIBLE);
+            //TODO: fer sols fins elpunt mes alt?
+            List<Entry> entries = carregaDadesGrafic(llistaPunts,posPuntMesAlt);
+            LineDataSet dataSet = new LineDataSet(entries, "Label");
+            dataSet.setColor(Color.GREEN);
+            dataSet.setDrawCircles(false);
+            chart.setBackgroundColor(Color.TRANSPARENT);
+            chart.setDrawGridBackground(false);
+            dataSet.setLineWidth(2.5f);
+            ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+            dataSets.add(dataSet);
+            LineData lineData = new LineData();
+            lineData.addDataSet(dataSet);
+            chart.setData(lineData);
+            chart.invalidate();
+
+
+            // create a data object with the datasets
+
+            LineData data = new LineData(xVals, dataSets);
+            chart.setData(data);
+
+            ///
             QueryNom queryNom = new QueryNom();
             queryNom.setVista(textViewNom);
             queryNom.execute(puntMesAltVisible.getLongitudUTM() + "", puntMesAltVisible.getLatitudUTM() + "", puntMesAltVisible.getHuso() + "");
         }
+    }
+
+    private ArrayList<Entry> carregaDadesGrafic(ArrayList<Punt> punts, int posPuntMesAlt) {
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0;i< punts.size() && i< posPuntMesAlt;i++) {
+            // turn your data into Entry objects
+            entries.add(new Entry((int)llistaPunts.get(i).getAltura(), i));
+        }
+        return entries;
     }
 
 
